3b020fe297b7b560bf0c7294ed87414deb4a5a4d
nishka322 <nishka322@yandex.ru>
2026-02-02 10:05:49 +0300

    гыу JsonCreator

--- stats-service/stats-dto/src/main/java/ru/practicum/stats/dto/CreateEndpointHitDto.java
+++ stats-service/stats-dto/src/main/java/ru/practicum/stats/dto/CreateEndpointHitDto.java
@@ -1,29 +1,37 @@
 package ru.practicum.stats.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Data
 @Builder
 public class CreateEndpointHitDto {
 
-    @NotBlank
-    @Size(max = 255)
-    private String app;
-    @NotBlank
-    @Size(max = 255)
-    private String uri;
-    @NotBlank
-    @Size(max = 255)
-    private String ip;
-    @NotNull
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime timestamp;
+    private static final DateTimeFormatter FORMATTER =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
+    private final String app;
+    private final String uri;
+    private final String ip;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private final LocalDateTime timestamp;
+
+    @JsonCreator
+    public CreateEndpointHitDto(
+            @JsonProperty("app") String app,
+            @JsonProperty("uri") String uri,
+            @JsonProperty("ip") String ip,
+            @JsonProperty("timestamp") LocalDateTime timestamp) {
+        this.app = app;
+        this.uri = uri;
+        this.ip = ip;
+        this.timestamp = timestamp;
+    }
 }
